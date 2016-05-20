0c33c38dc4659242c7333b36498eae86b884f8d0
Shailendra Chauhan <pro.shailendra@gmail.com>
2016-05-20 08:39:24 +0530

    published package
    published package

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * Adds commas to a number
+ * @param {number} number
+ * @param {string} locale
+ * @return {string}
+ */
+module.exports = function(number, locale) {
+    return number.toLocaleString(locale);
+};
